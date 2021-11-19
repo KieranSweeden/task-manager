89cbfc47073d8129a003248db6f718033a5527e5
Kieran Sweeden <kieransweeden@gmail.com>
2021-11-19 18:48:09 +0000

    Add register template

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from flask_pymongo import PyMongo
 # To find documents from MongoDB we need to render the ObjectId
 from bson.objectid import ObjectId
 
+# Get werzeug's security features
+from werkzeug.security import generate_password_hash, check_password_hash
+
 # Only import the env file if it exists within the directory
 if os.path.exists("env.py"):
     import env
@@ -30,6 +33,10 @@ mongo = PyMongo(app)
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
